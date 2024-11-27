af73ad7a2d5cdd75505417c03f4c6127f62870e2
Gabriel da Silva <gabriel.rodrigues106@fatec.sp.gov.br>
2024-11-27 15:15:13 +0000

    endereco

--- App/src/App.java
+++ App/src/App.java
@@ -1,6 +1,8 @@
 import meuPacote.aluno;
+import meuPacote.endereco;
 public class App {
     public static void main(String[] args) throws Exception {
         aluno a = new aluno();
+        endereco e = nem endereco();
     }
 }
